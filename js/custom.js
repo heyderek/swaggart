678866c8dea979d07c5e4748a1d5fd591bf20cd4
Derek <heyderek@dereknelson.net>
2012-12-04 14:44:42 -0800

    Added animations for the slideshow.

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
   //The main (front page) slideshow
+  $('.caption').hide();
   $('.featured-content .flexslider').flexslider({
     animation: "slide",
-    controlNav: false
+    controlNav: false,
+    start: function(animatingTo){
+      $('.caption').fadeIn(1000);
+    },
+    after: function(animatingTo){
+      $('.caption').fadeIn(1000);
+    },
+    before: function(animatingTo){
+      $('.caption').hide();
+    }
   });
   //Activate Flexslider for use in the footer to display recent posts.
   $('footer .flexslider').flexslider({
