6f75dcd1d37b1b1cff9e14f712be46d9ac97a578
Derek <heyderek@dereknelson.net>
2012-12-04 13:29:05 -0800

    Adjusted speed for footer slider.

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
+  //The main (front page) slideshow
   $('.featured-content .flexslider').flexslider({
     animation: "slide",
     controlNav: false
   });
+  //Activate Flexslider for use in the footer to display recent posts.
   $('footer .flexslider').flexslider({
     animation: "slide",
     animationLoop: false,
+    slideshowSpeed: 3000,
+    animationSpeed: 300,
     controlNav: false
   });
+  //Move the controls container within the dom.
   $('footer .flex-direction-nav').html(function(){
     $(this).appendTo('.footer-nav-container');
   });
