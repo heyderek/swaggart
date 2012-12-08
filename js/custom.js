b1816b6a69c5477b18c08dfb06f16e98e43d9c64
Derek <heyderek@dereknelson.net>
2012-12-08 10:18:07 -0800

    Added more scripting for navigation.

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     animation: "slide",
     controlNav: false,
     start: function(animatingTo){
-      $('.caption').fadeIn(1000);
+      $('.caption').fadeIn(3000);
     },
     after: function(animatingTo){
-      $('.caption').fadeIn(1000);
+      $('.caption').fadeIn(3000);
     },
     before: function(animatingTo){
       $('.caption').hide();
@@ -25,23 +25,35 @@ $(document).ready(function(){
   $('footer .flex-direction-nav').html(function(){
     $(this).appendTo('.footer-nav-container');
   });
+  
+  
+  
+  
   //Navigation
   $('.menu li > a').click(function(){
     var child = $(this).parent().find('.menu-overlay');
+    var coverUp = $(this).parents().find('.cover');
+    
+          $(this).parents().find('.cover').remove();
 
     if(child.length > 0){
-      $('.featured-content .flexslider .flex-viewport').fadeTo(100, '.45');
+      $('<div class="cover"></div>').slideDown(300).appendTo('.page-content, .featured-content');
     }
     $('.close').click(function(){
-      $('.featured-content .flexslider .flex-viewport').css('opacity', '1');
-      $('.menu-overlay').slideUp();
+      $('.menu-overlay').slideUp(300);
+      $('.cover').slideUp(300, function(){
+        $(this).remove();
+      })
     });
     if($(this).next()){
-      $('.menu-overlay').slideUp();
-      $('.featured-content .flexslider .flex-viewport').css('opacity', '1');
-      $(this).next().slideDown();
+      $('.menu-overlay').slideUp(300);
+      $(this).next().slideDown(300);
     }else{
-      $(this).next().slideUp();
+      $(this).next().slideUp(300);
     }
   });
+  
+  
+  
+  
 });
