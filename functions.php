63482aab68551cacae32aa5981e8fe262a07ea01
Derek <heyderek@dereknelson.net>
2012-12-11 09:25:46 -0800

    Added options framework.

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+add_theme_support('post-thumbnails');
+
 //Add Navigation
 function setup_theme_features(){
   register_nav_menus(array(
@@ -11,17 +13,56 @@ add_action('after_setup_theme', 'setup_theme_features');
 
 //Extend Navigation with Custom Walker Class.  Add container if there is more than one child menu item.
 class swaggart_mega_menu extends Walker_Nav_Menu {
-  // add classes to ul sub-menus
-  function start_lvl( &$output, $depth ) {
-      // depth dependent classes
-      $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
-      $display_depth = ( $depth >= 1); // because it counts the first submenu as 0
-      // build html
-      $output .= "\n" . $indent . '<div class="menu-overlay">' . "\n" . $indent . '<ul>' . "\n";
-  }
-  //Close the .menu-overlay div
-  function end_lvl( &$output, $depth = 0, $args = array() ) {
-    $indent = str_repeat("\t", $depth);
-    $output .= "$indent</ul>\n</div>\n";
-  }
+    function start_el(&$output, $item, $depth, $args) {
+        global $wp_query;
+
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+        $class_names = $value = '';
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
+        $class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+        $output .= $indent . '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
+
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        
+        // get user defined attributes for thumbnail images
+        $attr_defaults = array( 'class' => 'nav_thumb' , 'alt' => esc_attr( $item->attr_title ) , 'title' => esc_attr( $item->attr_title ) );
+        $attr = isset( $args->thumbnail_attr ) ? $args->thumbnail_attr : '';
+        $attr = wp_parse_args( $attr , $attr_defaults );
+ 
+        $item_output = $args->before;
+        
+        // thumbnail image output
+        $item_output .= ( isset( $args->thumbnail_link ) && $args->thumbnail_link ) ? '<a' . $attributes . '>' : '';
+        $item_output .= apply_filters( 'menu_item_thumbnail' , ( isset( $args->thumbnail ) && $args->thumbnail ) ? get_the_post_thumbnail( $item->object_id , ( isset( $args->thumbnail_size ) ) ? $args->thumbnail_size : 'thumbnail' , $attr ) : '' , $item , $args , $depth );		
+        $item_output .= ( isset( $args->thumbnail_link ) && $args->thumbnail_link ) ? '</a>' : '';
+
+        // menu link output
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+
+        // menu description output based on depth
+        $item_output .= ( $args->desc_depth >= $depth ) ? '<br /><span class="sub">' . $item->description . '</span>' : '';
+
+        // close menu link anchor
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
 }
+
+/*
+//Load Options Framework
+if ( !function_exists( 'optionsframework_init' ) ) {
+	define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/inc/' );
+	require_once dirname( __FILE__ ) . '/inc/options-framework.php';
+}
+*/
