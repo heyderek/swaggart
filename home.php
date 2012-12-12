63eb7984a1dfc2e97f7480ee6b265ed208cff746
Derek <heyderek@dereknelson.net>
2012-12-11 18:52:06 -0800

    Added page and home to repo.

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<section class="featured-content">
+  <div class="flexslider">
+    <ul class="slides">
+      <li>
+        <img src="<?php bloginfo('template_url'); ?>/images/slide_1.png" />
+        <figure class="caption left bottom content">
+          <h3>The Sky is the Limit.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="page.html" class="big-button"><span>Learn More</span>about our skyline.</a>
+        </figure><!-- /.caption -->
+      </li>
+      <li>
+        <img src="<?php bloginfo('template_url'); ?>/images/slide_2.png" />
+          <figure class="caption left top blktnt content">
+          <h3>Commercial Building</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="page.html" class="big-button"><span>Learn More</span>about our skyline.</a>
+        </figure><!-- /.caption -->
+      </li>
+    </ul><!-- /.slides -->
+  </div>
+</section><!-- /.featured-content -->
+<?php get_footer(); ?>
