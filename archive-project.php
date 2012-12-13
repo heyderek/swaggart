911248c52c752a3080d07108287671223fccce53
Derek <heyderek@dereknelson.net>
2012-12-12 19:10:37 -0800

    Clenaed up code.

--- archive-project.php
+++ archive-project.php
@@ -18,20 +18,20 @@
     </section><!-- /.secondary -->
     <section class="primary content">
     <?php 
-          $post_type = 'project';
-          // Get all the taxonomies for this post type
-          $taxonomies = get_object_taxonomies( (object) array( 'post_type' => $post_type ) );
-          foreach( $taxonomies as $taxonomy ) : 
-              // Gets every "category" (term) in this taxonomy to get the respective posts
-              $terms = get_terms( $taxonomy );
-              foreach( $terms as $term ) : 
-                  $posts = new WP_Query( "taxonomy=$taxonomy&term=$term->slug&posts_per_page=1" );
-                  if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post();
-                      //Do you general query loop here  
-                      echo '<p><h3>' . $term->name . '</h3>' . the_post_thumbnail('thumbnail') . '</p>';
-                  endwhile; endif;
-              endforeach;
-          endforeach;    
+      $post_type = 'project';
+      // Get all the taxonomies for this post type
+      $taxonomies = get_object_taxonomies( (object) array( 'post_type' => $post_type ) );
+      foreach( $taxonomies as $taxonomy ) : 
+          // Gets every "category" (term) in this taxonomy to get the respective posts
+          $terms = get_terms( $taxonomy );
+          foreach( $terms as $term ) : 
+              $posts = new WP_Query( "taxonomy=$taxonomy&term=$term->slug&posts_per_page=1" );
+              if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post();
+                  //Do you general query loop here  
+                  echo '<p><h3>' . $term->name . '</h3>' . the_post_thumbnail('thumbnail') . '</p>';
+              endwhile; endif;
+          endforeach;
+      endforeach;    
     ?>
     </section><!-- /.primary -->
   </div>
