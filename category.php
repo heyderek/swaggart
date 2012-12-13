a91b03f3a0824619201710320552da48b8c22189
Derek <heyderek@dereknelson.net>
2012-12-13 10:42:24 -0800

    Added some style changes for index and category pages.

--- category.php
+++ category.php
@@ -11,7 +11,7 @@
     </section><!-- /.secondary -->
     <section class="primary content">
     <header>
-      <h2><?php printf( __( '%s', '' ),  single_cat_title( '', false ) ); ?></h2>
+      <h2 class="cat-heading"><?php printf( __( '%s', '' ),  single_cat_title( '', false ) ); ?></h2>
       <?php
       $category_description = category_description();
         if ( ! empty( $category_description ) )
@@ -20,6 +20,7 @@
     <?php while ( have_posts() ) : the_post(); ?>
       <article class="posts">
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <h2><?php the_date('F Y'); ?></h2>
         <?php the_content(); ?>
       </article>
       <?php endwhile; ?>
