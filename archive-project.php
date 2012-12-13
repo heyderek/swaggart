2e4fec777ea9584edf9dbe2e34970f68a4736bbf
Derek <heyderek@dereknelson.net>
2012-12-12 19:06:26 -0800

    Fixed tax query.

--- archive-project.php
+++ archive-project.php
@@ -18,46 +18,21 @@
     </section><!-- /.secondary -->
     <section class="primary content">
     <?php 
-      $args = array( 'taxonomy' => 'Classification' );
-      $terms = get_terms('Classification', $args);
-      $count = count($terms); $i=0;
-
-      if ($count > 0) {
-        foreach ($terms as $term) {
-          
-          global $post;
-          $term_item = $term->slug;
-          $args = array(
-            'tax_query' => array(
-              'taxonomy'=>'classification', 
-              'terms'=> 'agriculture'
-              )
-          );
-          $posts_array = get_posts($args);
-          
-          foreach($posts_array as $solo_post){
-            $solo_title = $solo_post->post_title;
-          }
-          
-          $term_list .= '<p><a href="/project/' . $term->slug . '" title="' . sprintf(__('View all post filed under %s', 'my_localization_domain'), $term->name) . '">' . $term->name .'</a>&nbsp;'. $solo_title . '&nbsp;' . $term_item .'</p>';
-
-        }
-        echo $term_list;
-      }
-     ?>
-
-<!--
-       <?php
-         global $post;
-          $args = array('post_type' => 'project');
-          $posted = get_posts($args); 
-          foreach($posted as $post_one) {
-            $title .= $post_one->post_title;
-          }
-            
-          echo $title;
-        ?> 
--->
+          $post_type = 'project';
+          // Get all the taxonomies for this post type
+          $taxonomies = get_object_taxonomies( (object) array( 'post_type' => $post_type ) );
+          foreach( $taxonomies as $taxonomy ) : 
+              // Gets every "category" (term) in this taxonomy to get the respective posts
+              $terms = get_terms( $taxonomy );
+              foreach( $terms as $term ) : 
+                  $posts = new WP_Query( "taxonomy=$taxonomy&term=$term->slug&posts_per_page=1" );
+                  if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post();
+                      //Do you general query loop here  
+                      echo '<p><h3>' . $term->name . '</h3>' . the_post_thumbnail('thumbnail') . '</p>';
+                  endwhile; endif;
+              endforeach;
+          endforeach;    
+    ?>
     </section><!-- /.primary -->
   </div>
 </section><!-- /.page-content -->
