0ed22d41edc5eb931e6b36a80fc73aa2bcd4ac3e
Derek <heyderek@dereknelson.net>
2012-12-13 16:54:39 -0800

    Added template for custom taxonomy.

---
+++ taxonomy-classification.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<section class="page-content">
+  <div class="headerimg">
+    <img src="<?php bloginfo('template_url'); ?>/images/header.png" />
+  </div><!-- /.headerimg -->
+  <div class="wrapper">
+    <section class="secondary">
+      <?php do_action('before_sidebar'); ?>
+        <?php if(! dynamic_sidebar('sidebar-1')) : ?>
+      <?php endif; ?>
+    </section><!-- /.secondary -->
+    <section class="primary content">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <article class="posts">
+        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <?php the_content(); ?>
+      </article>
+      <?php endwhile; ?>
+    </section><!-- /.primary -->
+  </div>
+</section><!-- /.page-content -->
+<?php get_footer(); ?>
