765040f9f20bab82e46889395a0148c368355eed
Derek <heyderek@dereknelson.net>
2012-12-18 10:17:06 -0800

    Added social media links.

--- footer.php
+++ footer.php
@@ -18,10 +18,10 @@
           </div><!-- /.flexslider -->
           <div class="footer-nav-container"></div><!-- /.footer-nav-container -->
           <ul class="social">
-            <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/linked_in.png" /></a></li>
-            <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/twitter.png" /></a></li>
-            <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/facebook.png" /></a></li>
-            <li><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/email.png" /></a></li>
+            <li><a href="http://www.linkedin.com/company/2683901?trk=tyah" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/linked_in.png" /></a></li>
+            <li><a href="https://twitter.com/SwaggartBros" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/twitter.png" /></a></li>
+            <li><a href="https://www.facebook.com/pages/Swaggart-Brothers-Inc/445260195519020?ref=ts&fref=ts" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/facebook.png" /></a></li>
+            <li><a href="http://swaggart.versatilehosting.com/contact-us/"><img src="<?php bloginfo('template_url'); ?>/images/email.png" /></a></li>
           </ul><!-- /.social -->
           <footer class="credits">
             <p>&copy; 2012 Swaggart Brothers. Inc.</p>
