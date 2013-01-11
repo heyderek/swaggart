be31b6cdd2e26cd53bf5d62dd3043dda28d3052f
Derek <heyderek@dereknelson.net>
2013-01-11 11:37:31 -0800

    Fixes in the footer for links, etc.

--- footer.php
+++ footer.php
@@ -21,11 +21,11 @@
             <li><a href="http://www.linkedin.com/company/2683901?trk=tyah" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/linked_in.png" alt="Swaggart on LinkedIn icon" /></a></li>
             <li><a href="https://twitter.com/SwaggartBros" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/twitter.png" alt="Swaggart on Twitter icon" /></a></li>
             <li><a href="https://www.facebook.com/pages/Swaggart-Brothers-Inc/445260195519020?ref=ts&fref=ts" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/facebook.png" alt="Swaggart on Facebook icon" /></a></li>
-            <li><a href="http://swaggart.versatilehosting.com/contact-us/"><img src="<?php bloginfo('template_url'); ?>/images/email.png" alt="Conact Swaggart icon" /></a></li>
+            <li><a href="<? echo home_url('/'); ?>contact-us/"><img src="<?php bloginfo('template_url'); ?>/images/email.png" alt="Conact Swaggart icon" /></a></li>
           </ul><!-- /.social -->
-          <footer class="credits">
+          <section class="credits">
             <p>&copy; 2012 Swaggart Brothers. Inc.</p>
-          </footer><!-- /.credits -->
+          </section><!-- /.credits -->
           <?php wp_footer(); ?>
         </footer><!-- /.wrapper -->
       </section><!-- /.colophon -->
