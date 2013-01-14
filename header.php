f0e6c385890c45b2271e132a65d60577bfa36e79
Derek <heyderek@dereknelson.net>
2013-01-14 12:51:53 -0800

    FIxed title issue.

--- header.php
+++ header.php
@@ -11,20 +11,7 @@
     <script src="<?php bloginfo('template_url'); ?>/js/prefixfree.min.js" charset="utf-8"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/flexslider.js" charset="utf-8"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/custom.js" charset="utf-8"></script>
-    <title><?php
-      //Title based on page.
-      global $page, $paged;
-      wp_title( '|', true, 'right' );
-      // Add the blog name.
-      bloginfo( 'name' );
-      // Add the blog description for the home/front page.
-      $site_description = get_bloginfo( 'description', 'display' );
-      if ( $site_description && ( is_home() || is_front_page() ) )
-      	echo " | $site_description";
-      // Add a page number if necessary:
-      if ( $paged >= 2 || $page >= 2 )
-      	echo ' | ' . sprintf( __( 'Page %s', 'toolbox' ), max( $paged, $page ) ); ?>
-    </title>
+    <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
   </head>
   <body>
